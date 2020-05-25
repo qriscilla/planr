d8937cef8640fb3473f082f666625ad1409851f4
qriscilla <pri.whang@gmail.com>
2020-05-25 15:09:08 -0400

    Homepage completed with horizontal scroll

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import Navbar from '../layout/Navbar';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import Footer from '../layout/Footer';
 
 function Home() {
     return (
         <div>
+
             <Navbar />
 
-            <Grid container className='home' justify='center'>
-                <Grid item xs={11} sm={5} lg={3} >
-                    <div className='home_bucket'>
-                        <div className='home_title'>TO DO</div>                        
+            <div className='home'>
+
+                <section class="home_buckets">
+
+                    <div class="home_bucket">
+                        <div className='home_title'>TO DO</div>
                     </div>
-                </Grid>
-                <Grid item xs={11} sm={5} lg={3}>
-                    <div className='home_bucket'>
-                        <div className='home_title'>IN PROGRESS</div>                        
-                    </div>                </Grid>
-                <Grid item xs={11} sm={5} lg={3}>
-                    <div className='home_bucket'>
-                        <div className='home_title'>COMPLETED</div>                        
+                    <div class="home_bucket">
+                        <div className='home_title'>TO DO</div>
                     </div>
-                </Grid>
-            </Grid>
+                    <div class="home_bucket">
+                        <div className='home_title'>TO DO</div>
+                    </div>
+                    <div class="home_bucket">
+                        <div className='home_title'>TO DO</div>
+                    </div>
+                    <div class="home_bucket">
+                        <div className='home_title'>TO DO</div>
+                    </div>
+                    <div class="home_bucket">
+                        <div className='home_title'>TO DO</div>
+                    </div>
+
+                </section>
+
+            </div>
 
             <Footer />
+
         </div>
     )
 }
