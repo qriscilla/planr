76cb1c50edc00d38b99d3811ecb3bc0cb7c3e2b4
qriscilla <pri.whang@gmail.com>
2021-02-06 15:12:09 -0600

    Updates

---
+++ precache-manifest.e089d38113b55baba00c75f9f52839a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba119c52897adf9d7dc5d8bcec1fc671",
+    "url": "/planr/index.html"
+  },
+  {
+    "revision": "25d1ff511466954f1d2f",
+    "url": "/planr/static/css/main.c6500ba7.chunk.css"
+  },
+  {
+    "revision": "365406dcfcf74c6364f7",
+    "url": "/planr/static/js/2.8824c992.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/planr/static/js/2.8824c992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d1ff511466954f1d2f",
+    "url": "/planr/static/js/main.bb8936af.chunk.js"
+  },
+  {
+    "revision": "5996f74504b90e65a92a",
+    "url": "/planr/static/js/runtime-main.e1d75e34.js"
+  },
+  {
+    "revision": "825956f78da5ac68978fdf1ebfd5542b",
+    "url": "/planr/static/media/logo84909C.825956f7.svg"
+  },
+  {
+    "revision": "a488cdc51e048874697aad2ea61eecbc",
+    "url": "/planr/static/media/logoB3ACA7.a488cdc5.svg"
+  }
+]);
